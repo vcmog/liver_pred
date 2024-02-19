59ec652548081acb60324dc27b64b928f99a45a1
vcmog <scvcm@leeds.ac.uk>
2024-02-19 21:45:15 +0000

    Fix errors on slices

--- liver_pred/utils/cohort_processing.py
+++ liver_pred/utils/cohort_processing.py
@@ -11,9 +11,6 @@ from functions import run_sql_from_txt, load_sql_from_text
 import config
 
 
-# from sklearn.linear_model import LogisticRegression
-
-# import seaborn as sns
 import matplotlib.pyplot as plt
 
 from PropensityScoreMatcher import PropensityScoreMatcher
@@ -25,7 +22,7 @@ data_dir = config.data_dir  # project_dir / "data"
 sql_query_dir = config.sql_query_dir  # utils_dir / "SQL queries"
 output_dir = config.output_dir  # project_dir / "outputs"
 
-# Load data ######
+# Load data #
 
 # Setup postrges connection
 pg_engine = create_engine(
@@ -34,12 +31,14 @@ pg_engine = create_engine(
 
 
 # Load Cases
+print("Loading cases...")
 if Path(data_dir / "input/cases.csv").exists():
     cases = pd.read_csv(data_dir / "input/cases.csv")
 else:
     cases = load_sql_from_text("liver_cancer_patients.txt", engine=pg_engine)
     cases.to_csv(data_dir / "input/cases.csv")
 # Load Controls
+print("Loading controls...")
 if Path(data_dir / "input/controls.csv").exists():
     controls = pd.read_csv(data_dir / "input/controls.csv")
 else:
@@ -47,6 +46,7 @@ else:
     controls.to_csv(data_dir / "input/controls.csv")
 
 # Load characteristics of MIMICIV cohort
+print("Loading characteristics...")
 if Path(data_dir / "input/characteristics.csv").exists():
     characteristics = pd.read_csv(data_dir / "input/characteristics.csv")
 else:
@@ -60,23 +60,27 @@ else:
 # Match cohort #
 
 if config.perform_matching:
-    case_chars = characteristics[characteristics["hadm_id"].isin(cases["hadm_id"])]
+    print("Matching cohort...")
+    case_chars = characteristics[
+        characteristics["hadm_id"].isin(cases["hadm_id"])
+    ].copy()
     control_chars = characteristics[
         characteristics["subject_id"].isin(controls["subject_id"])
-    ]
+    ].copy()
     # sample random admission for each patient so duplicate patients are included
     control_chars = (
         control_chars.groupby("subject_id")
         .apply(lambda x: x.sample(1))
         .reset_index(drop=True)
+        .copy()
     )
 
     # option for number of matches
     no_matches = config.no_matches
 
     # Add outcome column to characteristics
-    case_chars.loc[:, "outcome"] = 1
-    control_chars.loc[:, "outcome"] = 0
+    case_chars["outcome"] = 1
+    control_chars["outcome"] = 0
 
     matcher = PropensityScoreMatcher(
         case_chars,
@@ -99,7 +103,7 @@ if config.perform_matching:
     )
     plt.show()
     matcher.match(nmatches=5)
-
+    print("Matching complete")
     matched_data = matcher.matched_data
     cohort_ids = matched_data[["subject_id", "hadm_id", "outcome"]]
     cohort_ids = cohort_ids.join(cases.set_index("hadm_id")["index_date"], on="hadm_id")
