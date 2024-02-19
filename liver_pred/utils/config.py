12bfade57434b3e6666a87f29275fccb354b182d
vcmog <scvcm@leeds.ac.uk>
2024-02-19 12:35:33 +0000

    Create config file with names of directories

---
+++ liver_pred/utils/config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+utils_dir = Path(__file__).parent.absolute()
+project_dir = utils_dir.parent.parent.absolute()
+data_dir = project_dir / "data"
+sql_query_dir = utils_dir / "SQL queries"
+output_dir = project_dir / "outputs"
