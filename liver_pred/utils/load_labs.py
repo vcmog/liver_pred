5479284b5410b5d6ecd0959afd67faf838d16ddc
vcmog <scvcm@leeds.ac.uk>
2024-02-20 14:59:38 +0000

    Add completion print

--- liver_pred/utils/load_labs.py
+++ liver_pred/utils/load_labs.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 # import psycopg2
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, types
 
 from pathlib import Path
 
@@ -17,5 +17,13 @@ import config
 
 data_dir = config.data_dir
 
-cohort_ids = pd.read_csv(data_dir / "interim/matched_cohort_ids.csv")
-print(cohort_ids.head())
+if config.perform_matching:
+    cohort_ids = pd.read_csv(data_dir / "interim/matched_cohort_ids.csv", index_col=0)
+else:
+    cohort_ids = pd.read_csv(data_dir / "interim/cohort_ids.csv", index_col=0)
+
+engine = create_engine(config.sql_connection_str)
+
+# Load data
+lab_data = load_sql_from_text("extract_labs.txt", engine=engine)
+print("Lab data loaded")
