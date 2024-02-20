36ca654dc08fab264a533b7208339fcc3a6ece18
vcmog <scvcm@leeds.ac.uk>
2024-02-20 16:57:33 +0000

    Add engine stirng

--- liver_pred/utils/config.py
+++ liver_pred/utils/config.py
@@ -1,11 +1,15 @@
 from pathlib import Path
 
+# Set pathnames
 utils_dir = Path(__file__).parent.absolute()
 project_dir = utils_dir.parent.parent.absolute()
 data_dir = project_dir / "data"
 sql_query_dir = utils_dir / "SQL queries"
 output_dir = project_dir / "outputs"
 
+
+# SQL options
+sql_connection_str = "postgresql+psycopg2://postgres:postgres@localhost:5432/mimic4"
 
 # Propensity score matching options
 
