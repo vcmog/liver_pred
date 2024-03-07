4cf5e27483c072c8556f0a9967b716f4a88d5526
vcmog <scvcm@leeds.ac.uk>
2024-03-07 17:21:51 +0000

    Add proximal and distal timepoints

--- liver_pred/utils/config.py
+++ liver_pred/utils/config.py
@@ -23,3 +23,20 @@ lab_aggregate = False
 lab_save_to_csv = True
 output_filename = "processed_lab_data.csv"
 output_path = data_dir / "interim"
+
+
+# Prediction model settings
+model_order = 1
+lead_time = 0
+observation_window = None
+
+# Window for 'current' lab tests in days around window
+current_window = 1
+
+# Window for binning historical lab tests in days
+historical_window = 14
+bin_window = 7
+
+# Timepoints for proximal and distal features in days
+proximal_timepoint = 180
+distal_timepoint = 365
