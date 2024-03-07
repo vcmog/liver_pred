d7df2a16fc75f16782a44fe0f200d9679d18163d
vcmog <scvcm@leeds.ac.uk>
2024-03-07 12:26:25 +0000

    CHange options to config variables

--- liver_pred/utils/process_labs.py
+++ liver_pred/utils/process_labs.py
@@ -9,19 +9,22 @@ labs_file_path = config.data_dir / "interim/lab_events"
 labs_data = pd.read_csv(labs_file_path, index_col=0)
 
 # Now you can use the "labs_data" variable to work with the contents of the file
-print(labs_data.head())
 
 # Print the number of unique values in the "valueuom" column for each "itemid"
-print(
-    labs_data.groupby("itemid")["valueuom"]
-    .nunique()
-    .sort_values(ascending=False)
-    .head(10)
-)
+# print(
+#    labs_data.groupby("itemid")["valueuom"]
+#    .nunique()
+#    .sort_values(ascending=False)
+#    .head(10)
+# )
 
 
 processed_lab_data = process_lab_tests(
-    labs_data, threshold=0.2, save_to_csv=False, aggregate=False
+    labs_data,
+    threshold=config.lab_threshold,
+    save_to_csv=config.lab_save_to_csv,
+    aggregate=config.lab_aggregate,
 )
 
+print("Processed lab data:")
 print(processed_lab_data.head())
