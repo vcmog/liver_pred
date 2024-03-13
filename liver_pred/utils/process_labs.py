c0855c1b38c3d65ba0e95287f4f092bc6cdf371e
vcmog <scvcm@leeds.ac.uk>
2024-03-13 16:40:51 +0000

    Print subject_id count

--- liver_pred/utils/process_labs.py
+++ liver_pred/utils/process_labs.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
 # Import the "config" module
-import config
+import config as config
 from lab_pipeline.process_lab_tests import process_lab_tests
 
 # Load the file "labs" from the "config.data_dir" directory
 labs_file_path = config.data_dir / "interim/lab_events"
 labs_data = pd.read_csv(labs_file_path, index_col=0)
-
+cohort_ids = pd.read_csv(
+    config.data_dir / "interim/matched_cohort_ids.csv", index_col=0
+)
 # Now you can use the "labs_data" variable to work with the contents of the file
 
 # Print the number of unique values in the "valueuom" column for each "itemid"
@@ -18,7 +20,7 @@ labs_data = pd.read_csv(labs_file_path, index_col=0)
 #    .head(10)
 # )
 
-
+print("Number of subject_ids before processing:", labs_data["subject_id"].nunique())
 processed_lab_data = process_lab_tests(
     labs_data,
     threshold=config.lab_threshold,
@@ -28,6 +30,15 @@ processed_lab_data = process_lab_tests(
     output_path=config.output_path,
 )
 
+
+subjects = processed_lab_data["subject_id"].nunique()
+n_cases = sum(
+    cohort_ids[cohort_ids["subject_id"].isin(processed_lab_data["subject_id"])][
+        "outcome"
+    ]
+)
+print("Number of subject_ids after processing:", subjects)
+print("Number of cases after processing:", n_cases, "(", n_cases / subjects * 100, "%)")
 print("Processed lab data:")
 print(processed_lab_data.head())
 print("Number of Unique Variables pre-processing:")
